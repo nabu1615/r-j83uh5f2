01310e1274f85c5c20b71276c1792b576ea1f162
Javier Vargas <javier.vargas@globant.com>
2017-08-15 18:59:10 -0500

    create stringLength function

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			value: ''
+		}
+
+		this.stringLength = this.stringLength.bind(this);
+	}
+
+	stringLength(event) {
+		this.setState ({
+			value: event.target.value
+		})
+	}
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea value={this.state.value} onChange={this.stringLength} rows="3"></textarea>
+        <div className="counter">{this.state.value.length}</div>
       </div>
     );
   }
